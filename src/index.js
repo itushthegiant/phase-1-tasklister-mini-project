4b4195b1df6d6c49d1ec78e5de6fa6f79a47145a
etamar <etamar.levy@gmail.com>
2021-04-05 18:25:34 -0400

    Completed asignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const input = document.getElementById("new-task-description");
+    const task = input.value;
+    const ul = document.getElementById("tasks")
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(task));
+    ul.appendChild(li);
+  });
 });
+
